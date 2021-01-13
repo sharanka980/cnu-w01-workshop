54f21a65ac9092307f0a74812e4df7aa560dc39b
sharanka980 <77329784+sharanka980@users.noreply.github.com>
2021-01-13 14:34:17 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
-name = 'Josephine'
-age = 36
-
+name = 'Sharan'
+age = '21'
 print(f'Hi, my name is {name}, and I am {age} years old.')
+
+birthday = 'September 13'
+
+print(f'hi my name is {name}, and i am {age} years old, My birthday is the {birthday})
+
